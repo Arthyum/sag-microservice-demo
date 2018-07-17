8a5d0d62e998d15a65daf8b8045e39a91383e2e5
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2018-07-16 22:44:35 -0400

    updating with new helper package + new microservice runtime image

--- docker-swarm-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#docker stack deploy --compose-file docker-compose-swarm.yml sagmicroservicedemo
-docker stack deploy --compose-file=<(docker-compose --file docker-compose-swarm.yml config) sagmicroservicedemo
